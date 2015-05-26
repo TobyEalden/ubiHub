0e533372f0d5cb4d5de9dca2d678f6627673068f
Toby Ealden <toby.ealden@gmail.com>
2015-05-26 23:52:16 +0100

    Interim for demo

--- client/main.js
+++ client/main.js
@@ -7,8 +7,8 @@ Session.set("SIGFOX-GFTDesignEnviroMon01-850C",false);
 Meteor.subscribe(ubiDatasetsName, function() {
   ubiDatasets.find().forEach(function(doc) {
     console.log("adding feed: " + doc.descriptor);
-    if (!ubiDataFeeds.hasOwnProperty(doc._id)) {
-      ubiDataFeeds[doc._id] = new Meteor.Collection(doc.descriptor);
+    if (!ubiDataFeeds.hasOwnProperty(doc._id.toString())) {
+      ubiDataFeeds[doc._id.toString()] = new Meteor.Collection(doc.descriptor);
       Meteor.subscribe(doc.descriptor, function() {
         Session.set(doc.descriptor,true);
       });
